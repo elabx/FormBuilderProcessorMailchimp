97a8f5f5f0dbb244c0632bfebb8d9f739fd1282c
Eduardo San Miguel García <abx88as@gmail.com>
2023-10-21 13:09:09 -0600

    init repo

---
+++ FormBuilderProcessorMailchimpActionConfig.php
@@ -0,0 +1,31 @@
+<?php namespace ProcessWire;
+class FormBuilderProcessorMailchimpActionConfig extends ModuleConfig {
+  public function getDefaults() {
+    return array(
+      'api_key' => '',
+      'server' => '',
+    );
+  }
+  public function getInputfields() {
+    $inputfields = parent::getInputfields();
+
+    $f = $this->modules->get('InputfieldText');
+    $f->attr('name', 'api_key');
+    $f->label = 'API Key';
+    $f->required = true;
+    $inputfields->add($f);
+
+    $f = $this->modules->get('InputfieldText');
+    $f->attr('name', 'server');
+    $f->label = 'Server';
+    $f->required = true;
+    $inputfields->add($f);
+
+    $f = $this->modules->get('InputfieldCheckbox');
+    $f->attr('name', 'mailchimp_log');
+    $f->label = 'Log responses?';
+    $inputfields->add($f);
+
+    return $inputfields;
+  }
+}
